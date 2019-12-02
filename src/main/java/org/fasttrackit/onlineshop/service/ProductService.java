d2d04845f7a35511da6ab05cc145471f392e81fb
Ionela Palacean <ione.caia85@gmail.com>
2019-12-02 19:31:02 +0200

    added getProducts method

--- src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -3,49 +3,70 @@ package org.fasttrackit.onlineshop.service;
 import org.fasttrackit.onlineshop.domain.Product;
 import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.persistance.ProductRepository;
+import org.fasttrackit.onlineshop.transfer.GetProductsRequest;
 import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
     // IoC- Inversion of Control
 
-    private static final Logger LOGGER= LoggerFactory.getLogger(ProductService.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
 
     private final ProductRepository productRepository;
-  //Dependency injection
+
+    //Dependency injection
     @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-    public Product createProduct(SaveProductRequest request){
+    public Product createProduct(SaveProductRequest request) {
         LOGGER.info("Creating product {} ", request);
-        Product product=new Product();
+        Product product = new Product();
         product.setDescription(request.getDescription());
         product.setName(request.getName());
         product.setPrice(request.getPrice());
         product.setQuantity(request.getQuantity());
         product.setImageUrl(request.getImageUrl());
 
-       return  productRepository.save(product);
+        return productRepository.save(product);
 
     }
 
-    public Product getProduct(long id){
+    public Product getProduct(long id) {
 
-        LOGGER.info("Retrieving product {}", id );
+        LOGGER.info("Retrieving product {}", id);
 
         //using Optional
-      return productRepository.findById(id)
-              //lambda expression
-              .orElseThrow(()->new ResourceNotFoundException("Product" + id+ "does not exist."));
+        return productRepository.findById(id)
+                //lambda expression
+                .orElseThrow(() -> new ResourceNotFoundException("Product" + id + "does not exist."));
 
     }
+
+
+    public Page<Product> getProducts(GetProductsRequest request, Pageable pageable) {
+
+        LOGGER.info("Retrieving products: {}", request);
+        if (request != null && request.getPartialName() != null && request.getMinQuantity() != null) {
+            return productRepository.findByNameContainingAndQuantityGreaterThanEqual(
+                    request.getPartialName(), request.getMinQuantity(), pageable);
+        } else if (request != null && request.getPartialName() != null && request.getMinQuantity() != null) {
+
+            return productRepository.findByNameContaining(request.getPartialName(), pageable);
+        } else {
+            return productRepository.findAll(pageable);
+        }
+    }
+
+
 
     public Product updateProduct(long id, SaveProductRequest request) {
 
