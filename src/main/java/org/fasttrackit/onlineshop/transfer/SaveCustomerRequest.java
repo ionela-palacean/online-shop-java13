99cef975852886316374b8ca87a40b4337ce4de5
Ionela Palacean <ione.caia85@gmail.com>
2019-12-09 19:27:30 +0200

    added customer service

---
+++ src/main/java/org/fasttrackit/onlineshop/transfer/SaveCustomerRequest.java
@@ -0,0 +1,43 @@
+package org.fasttrackit.onlineshop.transfer;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+
+
+public class SaveCustomerRequest {
+
+
+
+@NotBlank
+private String firstName;
+@NotBlank
+private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "SaveCustomerRequest{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
